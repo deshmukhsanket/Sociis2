5870cb9c1525a0bbf5d74c53ea9790f249a5576b
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2018-08-18 01:56:47 +0530

    DONE Ledger Add Api

--- api/controllers/LedgerController.js
+++ api/controllers/LedgerController.js
@@ -1,3 +1,11 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+    makeLedgerEntry: function (req, res) {
+        if (req.body) {
+            Ledger.makeLedgerEntry(req.body, res.callback);
+        } else {
+            res.callback("No Data");
+        }
+    }
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/Ledger.js
+++ api/services/Ledger.js
@@ -6,9 +6,10 @@ var schema = new Schema({
         type: Schema.Types.ObjectId
     },
     productDetails: [{
-        productName: String,
+        productId: Schema.Types.ObjectId,
         quantity: Number,
-        CP: Number
+        CP: Number,
+        SP: Number
     }]
 
 });
@@ -19,5 +20,44 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('Ledger', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+    // Things to Include From Where BillNo Original Bill GST No OF Source Location.etc
+    makeLedgerEntry: function (data, callback) {
+        var Const = this(data);
+        Const.save(function (err, savedData) {
+            if (err) {
+                callback("Error In Saving");
+            } else {
+                async.each(data.productDetails, function (n, callback) {
+                    Product.findOne({
+                        _id: n.productId
+                    }).lean().exec(function (err, product) {
+                        if (err) {
+                            callback();
+                        } else {
+                            if (isNaN(product.totalQuantity) || product.totalQuantity == undefined) {
+                                product.totalQuantity = 0;
+                            }
+                            product.productDetails.push(n);
+                            product.totalQuantity = product.totalQuantity + n.quantity;
+                            Product.update({
+                                _id: product._id
+                            }, {
+                                productDetails: product.productDetails,
+                                totalQuantity: product.totalQuantity
+                            }).lean().exec(callback);
+                        }
+                    });
+                }, function (err, data) {
+                    if (err) {
+                        callback(err, null);
+                    } else {
+                        callback(null, "Modified Changes");
+                    }
+                });
+            }
+
+        });
+    }
+};
 module.exports = _.assign(module.exports, exports, model);
